239b955ceefd8e65b2516b8e4873e51e94e7b356
ElCresp0 <jacky8203@protonmail.com>
2023-11-10 00:22:09 +0100

    add filesystem functions for bluetooth tasks

--- Rejestrator/main/SDCardController.h
+++ Rejestrator/main/SDCardController.h
@@ -3,35 +3,40 @@
 
 #include "BicFunctions.h"
 
-//sd & memory
+// sd & memory
+#include <string>
 #include "driver/sdmmc_host.h"
 #include <SD_MMC.h>
 #include "esp_vfs_fat.h"
-#include "FS.h"                // File system for SD Card ESP32
-#include <EEPROM.h>   
+#include "FS.h" // File system for SD Card ESP32
+#include <EEPROM.h>
 
-class SDCardController {
+#define AVI_NAME_LENGTH 30
+
+class SDCardController
+{
 public:
     void initialize();
     void addToFile(camera_fb_t *frame);
     bool startFile();
     void closeFile(bool buttonPressed, uint32_t fileFramesTotalSize);
     void deleteOldFiles();
-    RTC_DS3231 rtc;
-private:  
-    // The following relate to the AVI file that gets created.
-    uint16_t fileFramesWritten      = 0;          // Number of frames written to the AVI file.
-    uint32_t fileStartTime          = 0;          // Used to calculate FPS.
-    uint32_t filePadding            = 0;          // Total padding in the file.
+    std::string listFiles();
+    FILE *getFileStream(std::string name, std::string mode);
+    // RTC_DS3231 rtc;
 
-    int AVI_NAME_LENGTH             = 30; 
+private:
+    // The following relate to the AVI file that gets created.
+    uint16_t fileFramesWritten = 0; // Number of frames written to the AVI file.
+    uint32_t fileStartTime = 0;     // Used to calculate FPS.
+    uint32_t filePadding = 0;       // Total padding in the file.
 
     FILE *aviFile;
-    FILE *idx1File;                               // Temporary file used to hold the index information
+    FILE *idx1File; // Temporary file used to hold the index information
     void writeIdx1Chunk();
     void calculateFps();
     void updateResolution();
-    void findOldestFile(File dir, char* oldestFileName);
+    void findOldestFile(File dir, char *oldestFileName);
 };
 
 #endif
