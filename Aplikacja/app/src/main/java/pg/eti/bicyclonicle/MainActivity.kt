ee29b91625a92a597ff50dfe0ebffa6e4ecbf719
ElCresp0 <104169465+ElCresp0@users.noreply.github.com>
2023-11-23 20:45:40 +0100

    Restore MainActivity.kt

--- Aplikacja/app/src/main/java/pg/eti/bicyclonicle/MainActivity.kt
+++ Aplikacja/app/src/main/java/pg/eti/bicyclonicle/MainActivity.kt
@@ -32,10 +32,6 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
         supportActionBar!!.hide()
     }
 }
